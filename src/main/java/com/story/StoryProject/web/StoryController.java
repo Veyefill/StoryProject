a332bd01c0288a0adc39c5dc1feb3d8f30be151c
Vi <bgz901@myy.haaga-helia.fi>
2023-11-24 19:37:30 +0200

    Added some pages, like the about, menu, new guest game and new guest warning.

---
+++ src/main/java/com/story/StoryProject/web/StoryController.java
@@ -0,0 +1,23 @@
+package com.story.StoryProject.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StoryController {
+	
+	@RequestMapping("/menu")
+	public String menu() {
+		return "menu";
+	}
+	
+	@RequestMapping("/newgameguest")
+	public String newgameguest() {
+		return "newgameguest";
+	}
+	
+	@RequestMapping("/about")
+	public String about() {
+		return "about";
+	}
+}
